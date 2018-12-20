ae45210605b518615feb716b18279ca5e585f678
14814 <1481454450@qq.com>
2018-12-20 16:15:57 +0800

    [新增]登陆功能

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,16 @@
 const router = require('koa-router')();
 const login = require('./admin/login');
 const user = require('./admin/user');
-
+const url = require('url');
 //配置中间件获取url
 router.use(async (ctx, next) =>{
     ctx.state.__HOST__ = 'http://' + ctx.request.header.host;
+    let pathName = url.parse(ctx.request.url).pathname;
     //权限判断是否登陆
     if(ctx.session.userinfo){
         await next();
     }else{
-        if(ctx.url === '/admin/login' || ctx.url === '/admin/login/doLogin'){
+        if(pathName === '/admin/login' || pathName === '/admin/login/doLogin' || pathName === '/admin/login/code'){
             await next();
         }else{
             ctx.redirect('/admin/login')
--- routes/admin/login.js
+++ routes/admin/login.js
@@ -2,20 +2,52 @@ const router = require('koa-router')();
 const tools = require('../../model/tools.js');
 const DB = require('../../model/db.js');
 
+const svgCaptcha = require('svg-captcha');//验证码
+
 router.get('/', async ctx => {
     await ctx.render('admin/login')
 });
+
 router.post('/doLogin', async ctx => {
     let userName = ctx.request.body.userName;
     let password = ctx.request.body.password;
-    let result = await DB.find('admin',{userName:userName,password:tools.md5(password)});
+    let code = ctx.request.body.code;
+    if (code.toLocaleLowerCase() === ctx.session.code.toLocaleLowerCase()) {
+        let result = await DB.find('admin', {userName: userName, password: tools.md5(password)});
 
-    if(result){
-        ctx.session.userinfo = result[0];
-        ctx.redirect(ctx.state.__HOST__ + '/admin')
-    }else{
-        console.log(result)
+        if (result) {
+            ctx.session.userinfo = result[0];
+            ctx.redirect(ctx.state.__HOST__ + '/admin')
+        } else {
+            ctx.render('admin/error', {
+                message: '用户名或密码错误',
+                redirect: ctx.state.__HOST__ + '/admin/login'
+            })
+        }
+
+    } else {
+        ctx.render('admin/error', {
+            message: '验证码校验失败',
+            redirect: ctx.state.__HOST__ + '/admin/login'
+        })
     }
+
 });
 
+//验证码
+router.get('/code', async ctx => {
+    const captcha = svgCaptcha.create({
+        size: 4,
+        fontSize: 30,
+        width: 100,
+        height: 40,
+        background: ''
+    });
+
+    ctx.session.code = captcha.text;//保存验证码
+
+    ctx.response.type = 'image/svg+xml';//设置响应头
+
+    ctx.body = captcha.data;
+});
 module.exports = router.routes();
