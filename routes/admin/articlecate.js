f09741b8433ea693b514ae503a4da0e3668878d8
14814 <1481454450@qq.com>
2018-12-25 21:36:14 +0800

    [新增]分类列表增加，修改

--- routes/admin/articlecate.js
+++ routes/admin/articlecate.js
@@ -10,11 +10,40 @@ router.get('/', async ctx => {
 });
 
 router.get('/add', async ctx => {
-    await ctx.render('admin/user/add')
+    let result = await DB.find('articlecate',{pid:'0'});
+    await ctx.render('admin/articlecate/add',{
+        catelist:result
+    })
+});
+
+router.post('/doAdd', async ctx => {
+    let addData = ctx.request.body;
+    DB.insert('articlecate',addData);
+    ctx.redirect(ctx.state.__HOST__ + '/admin/articlecate')
 });
 
 router.get('/edit', async ctx => {
-    ctx.body = '编辑用户'
+    let id = ctx.query.id;
+    let result = await DB.find('articlecate',{_id:DB.getObjectId(id)});
+    let articlecate = await DB.find('articlecate',{pid:'0'});
+    await ctx.render('admin/articlecate/edit',{
+        list:result[0],
+        catelist:articlecate
+    })
+});
+
+router.post('/doEdit', async ctx => {
+    let editData = ctx.request.body;
+    let id = editData.id;
+    let title = editData.title;
+    let pid = editData.pid;
+    let keywords = editData.keywords;
+    let status = editData.status;
+    let description = editData.description;
+    await DB.update('articlecate',{_id:DB.getObjectId(id)},{
+        title,pid,keywords,status,description
+    });
+    ctx.redirect(ctx.state.__HOST__ + '/admin/articlecate')
 });
 
 router.get('/delete', async ctx => {
